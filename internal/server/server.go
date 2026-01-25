8ee866669f326f52d6b81a19bbed9cd6934bca81
GiovaneCardoso <giovane.ferreira0210@gmail.com>
2026-01-24 23:06:15 -0300

    separate admin route

--- internal/server/server.go
+++ internal/server/server.go
@@ -11,9 +11,13 @@ import (
 
 func StartServer() {
 	r := gin.Default()
+	adminTenantHandler := adminTenantHandler()
+	admin.RegisterAdminTentantRoutes(r, adminTenantHandler)
+	r.Run(":8080")
+}
+func adminTenantHandler() *handler.TenantHandler {
 	repository := memory.NewTenantInMemory()
 	service := service.NewTenantService(repository)
 	tenantHandler := handler.NewTenantHandler(service)
-	admin.RegisterAdminTentantRoutes(r, tenantHandler)
-	r.Run(":8080")
+	return tenantHandler
 }
